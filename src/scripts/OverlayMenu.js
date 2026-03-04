73fbbdc8a1a9e41394df592ef32d91c39c44af39
Islam <gamzatovislam314@gmail.com>
2026-03-04 21:03:00 +0300

    feat: ass Skills section and anchor navigation

--- src/scripts/OverlayMenu.js
+++ src/scripts/OverlayMenu.js
@@ -3,6 +3,7 @@ class OverlayMenu {
 		root: '[data-js-overlay-menu]', // В значении в одиночных кавычках указываем CSS селектор по атрибуту.
 		dialog: '[data-js-overlay-menu-dialog]',
 		burgerButton: '[data-js-overlay-menu-burger-button]',
+		menuLink: 'a[href^="#"]',
 	}
 
 	stateClasses = { // Здесь будут перечислены css классы состояний.
@@ -14,6 +15,7 @@ class OverlayMenu {
 		this.rootElement = document.querySelector(this.selectors.root)
 		this.dialogElement = this.rootElement.querySelector(this.selectors.dialog)
 		this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton)
+		this.menuLinks = this.dialogElement.querySelectorAll(this.selectors.menuLink)
 		this.bindEvents()
 	}
 
@@ -23,8 +25,17 @@ class OverlayMenu {
 		document.documentElement.classList.toggle(this.stateClasses.isLock) // Блокировка вертикального скролла.
 	}
 
+	onMenuLinkClick = () => {
+		this.burgerButtonElement.classList.remove(this.stateClasses.isActive)
+		this.dialogElement.open = false
+		document.documentElement.classList.remove(this.stateClasses.isLock)
+	}
+
 	bindEvents() { // В подобных методах в синтаксисе JS классов мы будем привязывать слушатели браузерных событий к DOM-элементам.
 		this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+		this.menuLinks.forEach(link => {
+			link.addEventListener('click', this.onMenuLinkClick)
+		})
 	}
 }
 
